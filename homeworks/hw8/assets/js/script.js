b1b9e970aa6b3d092c9b5fe93bd31e1672fbbbcf
tiffanytra <tiffanytra@berkeley.edu>
2026-02-01 14:40:34 -0800

    Clear repository files

--- homeworks/hw8/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function toggleSongs() {
-    let songsSecondHalf = document.getElementById("songs-second-half");
-    songsSecondHalf.classList.toggle("hidden");
-}
-
-document.getElementById("toggle-button").onclick = toggleSongs;
--- labs/lab8/script.js
+++
@@ -1,8 +0,0 @@
-function toggleTheme() {
-    let body = document.querySelector("body");
-    body.classList.toggle("dark-mode");
-}
-
-let button = document.querySelector("#toggleButton");
-
-button.onclick = toggleTheme;
